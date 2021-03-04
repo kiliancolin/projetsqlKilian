7d39361773bd21a9b4581b30a155dbd0a59b3d77
kiliancolin <kilian.colin@orange.fr>
2021-03-04 14:40:23 +0100

    update 5

--- projetNSI code python.py
+++ projetNSI code python.py
@@ -1,11 +1,13 @@
 def execute(requete,dico):
+    l = []
     import sqlite3
     conn = sqlite3.connect('imdb.db')
     c = conn.cursor()
-    c.execute(dico[1][1])
+    c.execute(dico[requete][1])
     for row in c:
-        print(row)
+        l.append(row)
     conn.close()
+    return l
 
 import tkinter
 import os
@@ -15,6 +17,12 @@ def afficher_table(table, titre ="", debut = 0, fin = None):
 		titre += "\n\n"
 	#print(titre + texte_table(table, debut, fin))
 	affichage(titre + texte_table(table, debut, fin), titre)
+    
+def texte_table(table, debut = 0, fin = None):
+  texte = ''
+  for i in range(len(table)):
+    texte = texte + str(table[i]) + '\n'
+  return texte
 
 def affichage(texte, titre = "Requêtes tables"):
 	root = tkinter.Tk()
@@ -96,100 +104,5 @@ stocker_requete(12,a)
 stocker_requete(14,a)
 stocker_requete(15,a)
 #print(afficher(a))
-afficher_table(execute(1,a))
-
-
-
-
-"""
-import sqlite3
-class database:
-    #https://docs.python.org/3/library/sqlite3.html
-    def __init__(self, base):
-        self.base = ""
-
-    def connexion(self):
-        self.con = sqlite3.connect(self.base)
-        self.cur = self.con.cursor()
-
-    def deconnexion(self):
-        self.con.close()
-
-    def fetch(self, sql):
-        self.connexion()
-        self.cur.execute(sql)
-        result = self.cur.fetchall()
-        self.deconnexion()
-        return result
-
-    def execute(self, sql):
-        self.connexion()
-        self.cur.execute(sql)
-        self.deconnexion()
-
-    def chargersql():
-        pass
-
-    def afficher_table(texte, titre = "Requêtes tables"):
-	
-	    Affiche un texte (résultat d'une requête)
-    	dans une fenêtre tkinter
-	    Auteurs: M CHOUCHI
-    	Arguments:
-	    	texte: str du texte à afficher
-		    titre: str du titre de la fenêtre
-	    Renvoi:
-		    rien
-	
-	    root = tkinter.Tk()
-    	root.title(str(titre))
-	    RWidth=root.winfo_screenwidth() - 100
-    	RHeight=root.winfo_screenheight() - 100
-	    root.geometry("%dx%d+50+0"%(RWidth, RHeight))
-    	text=tkinter.Text(root, wrap = 'none')
-	    scroll_x=tkinter.Scrollbar(text.master, orient='horizontal', command = text.xview)
-    	scroll_x.config(command = text.xview)
-	    text.configure(xscrollcommand = scroll_x.set)
-    	scroll_x.pack(side = 'bottom', fill = 'x', anchor = 'w')
-	    scroll_y = tkinter.Scrollbar(text.master)
-    	scroll_y.config(command = text.yview)
-	    text.configure(yscrollcommand = scroll_y.set)
-	    scroll_y.pack(side = tkinter.RIGHT, fill = 'y')
-    	text.insert("1.0", texte)
-	    text.pack(side = tkinter.LEFT, expand = True, fill = tkinter.BOTH)
-    	root.mainloop()
-    
-    def listedesrequetes():
-        pass
-
-    def infotable():
-        pass
-
-    def informations_base():
-        pass
-"""
-
-
-"""
-import sqlite3
-def database_connexion(db_file):
-    connexion = None
-    try:
-        connexion = sqlite3.connect(db_file)
-    exept Error as e:
-        return e
-
-    return connexion
-conn = database_connexion("data/imb.db")
-
-def execute_sql(connexion,sql):
-    cur = connexion.cursor()
-    cur.execute(sql)
-    raws = cur.fetchall()
-    for row in rows:
-        print(row)
-
-run_sql = execute_sql
-sql = "SELECT DISTINCT types FROM title_akas"
-run_sql(conn,sql)
-"""
+afficher_table(execute(1,a),a[1][0])
+#print(execute(1,a))
